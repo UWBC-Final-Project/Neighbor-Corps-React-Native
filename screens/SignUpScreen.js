894dae50a039cd379b572ca928347352f959b331
Kevin <kphyde@gmail.com>
2019-01-18 08:22:34 -0800

    copy paste task creation boiler and begin to customize to User creation

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,26 +1,136 @@
-import React, {Component} from 'react';
-import { ExpoConfigView } from '@expo/samples';
-import { Container, Header, Content, Form, Item, Input, Label, Text } from 'native-base';
+import React, { Component } from 'react';
+import { Button } from 'react-native';
+import { Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Item, Input } from 'native-base';
+import { AppRegistry, StyleSheet, View, TouchableHighlight } from 'react-native';
+import API from '../utils/API';
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  firstName: t.String,
+  lastName: t.String,
+  email: t.String,
+  userName: t.String,
+  password: t.String,
+  about: t.maybe(t.String),
+  terms: t.Boolean
+});
+
+const options = {
+  fields: {
+    terms: {
+      label: 'Agree to Terms',
+    },
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
 
 export default class SignUpScreen extends Component {
+
+  // Setting our component's initial state
+  state = {
+    tasks: [],
+    // NEED SPECIAL ATTENTION TO MAKE IT REFLECT MODEL
+    title: "",
+    description: "",
+    imageURL: "",
+    postion: "", // save what we grasp from Google map pinned location
+    // tags:[],
+    // postedBy: "",
+    // comments: [],
+    // postDate: "", 
+    // lastUpdated: ""
+  };
+
+  // When the component mounts, load all Tasks and save them to this.state.Tasks
+  componentDidMount() {
+    
+  }
+
+  // Handles updating component state when the Task types into the input field
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, use the API.saveTask method to save the Task data
+  // Then reload Tasks from the database
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    API.saveUser({
+      title: this.state.title,
+      description: this.state.description,
+      imageURL: this.state.imageURL,
+      postion: this.state.postion, // save what we grasp from Google map pinned location
+      // tags:[],
+      // postedBy:this.state.postedBy,
+      // comments: [],
+      // postDate: this.state.postDate, 
+      // lastUpdated:this.state.lastUpdated
+      // NEED SPECIAL ATTENTION TO MAKE IT REFLECT MODEL
+    })
+      .catch(err => console.log(err));
+
+  };
+
+  // supplied by tutorial for tcomb-form-native
+  handleSubmit = () => {
+    const value = this.refs.form.getValue(); // use that ref to get the form value
+    console.log('value: ', value);
+  }
+
+  onPress = () => {
+    // call getValue() to get the values of the form
+    var value = this.refs.form.getValue();
+    if (value) { // if validation fails, value will be null
+      console.log(value); // value here is an instance of Person
+    }
+  }
+
   render() {
     return (
+
       <Container>
         <Header />
         <Content>
-          <Text>SIGN UP SCREEN</Text>
-          <Form>
-            <Item floatingLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input />
-            </Item>
-          </Form>
+          <Form
+            ref="form"
+            type={User}
+          />
+          <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Save</Text>
+          </TouchableHighlight>
         </Content>
       </Container>
-    );
+
+    )
   }
+
 }
