8aaab8bd2641d1c20da9a7bc3ce577a481a8080b
PKaur <dgninder@gmail.com>
2019-01-20 17:56:42 -0800

    not to merge

---
+++ components/UserProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+// import { Linking, TouchableHighlight } from 'react-native';
+
+// creating component for user profile ~~PK
+export default UserProfile = (props) => {
+//   const { title, href, ingredients, thumbnail} = props.data;
+    return (
+    console.log("returning something")
+    );
+}
