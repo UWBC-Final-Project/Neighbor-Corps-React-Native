9d1f79cca8c3069cfbac8ff4dad68b34b3065dec
Kevin <kphyde@gmail.com>
2019-01-23 10:31:14 -0800

    insert Task conponent into SingleTaskScreen and set props correctly

--- screens/SingleTaskScreen.js
+++ screens/SingleTaskScreen.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import { View } from 'react-native';
 import Header from '../components/Header';
+import Task from '../components/Task';
 import { Image } from 'react-native';
 import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
 import API from '../utils/API';
@@ -32,7 +33,7 @@ export default class SingleTaskScreen extends Component {
         <Header page={this.state.page}/>
         <Content>
           {/* KPH Repeated via Copy/Paste here but would render with a Mapped return from the DB in the future */}
-          <Task taskProps={props} />
+          <Task taskProps={this.props} />
         </Content>
       </Container>
     );
