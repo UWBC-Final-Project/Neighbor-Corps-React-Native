18c0cd008690c23cfaf00a3c4e176cfaa4145bfa
Kevin <kphyde@gmail.com>
2019-01-30 11:18:09 -0800

    add font styles and logo file with positioning

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { ExpoConfigView } from '@expo/samples';
-import { Container,CardItem,ScrollView,Image, Content, Item, Input, Labelzz,  Header, Title, Button, Left, Right, Body, Center, Icon, Thumbnail, ImageBackground } from 'native-base';
-import { View, Text, Linking, TouchableHighlight, StyleSheet  } from 'react-native';
+import { Font } from 'expo';
+import { Container, CardItem, ScrollView, Content, Item, Input, Labelzz, Header, Title, Button, Left, Right, Body, Center, Icon, Thumbnail, ImageBackground } from 'native-base';
+import { Image, View, Text, Linking, TouchableHighlight, StyleSheet } from 'react-native';
 
 import Headerjs from '../components/Header';
 
@@ -14,56 +15,61 @@ const styles = StyleSheet.create({
     width: 327,
     height: 63,
     color: '#63a952',
-    fontFamily: 'Open Sans',
+    fontFamily: 'open-sans-light',
     fontSize: 46,
-    fontWeight: '300',
     lineHeight: 46,
+    top: 60,
   },
   lendA: {
     width: 333,
     height: 46,
     color: '#63a952',
-    fontFamily: 'Open Sans',
+    fontFamily: 'open-sans-regular',
     fontSize: 18,
-    fontWeight: '400',
     lineHeight: 46,
+    top: 36,
+  },
+  logo: {
+    width: 292,
+    height: 229,
+    top: 100,
   },
 })
-
 
 export default class WelcomeScreen extends Component {
   state = {
     page: "Welcome",
+    fontLoaded: false,
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'open-sans-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+      'open-sans-light': require('../assets/fonts/OpenSans-Light.ttf'),
+      'open-sans-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
   }
 
   render() {
     return (
-      <Container>
-        <Text style={styles.neighborCorps}>Neighbor Corps</Text>
-        <CardItem style={{ backgroundColor: '#52d1ed', height: '10%', width: '50%', marginLeft: '25%', marginTop: '10%' }}>
-          {/* <TouchableHighlight
-                onPress={() => Linking.openURL(href)}>
-            <Text style={{color: '#b354e9',padding:50 ,fontSize:20, fontWeight:'bold', fontFamily:'OpenSans-ExtraBold'}}>ABOUT</Text>
-            </TouchableHighlight> */}
-          </CardItem>
-          <CardItem style={{backgroundColor:'#52d1ed', height:'10%', width:'50%',marginLeft:'25%', marginTop:'10%' }}>
-          <TouchableHighlight
-                onPress={() => this.props.navigation.navigate('TasksScreen')}>
-            <Text style={{color: '#b354e9',padding:20 ,fontSize:20, fontWeight:'bold', fontFamily:'OpenSans-ExtraBold'}}>See All Tasks</Text>
-          </TouchableHighlight>
-          </CardItem>
-          <CardItem >
-          <TouchableHighlight style={{backgroundColor:'#52d1ed',height:'50%', width:'26%',marginLeft:'18%', marginTop:'10%' }}
-                onPress={() =>  this.props.navigation.navigate('LoginScreen')}>
-            <Text style={{ padding:15, fontSize:20, color: '#b354e9',fontWeight:'bold', fontFamily:'OpenSans-ExtraBold'}}>SignIn</Text>
-            </TouchableHighlight>
-    
-            <TouchableHighlight style={{backgroundColor:'#52d1ed', height:'50%', width:'26%',marginLeft:'15%', marginTop:'10%' }}
-            onPress={() =>  this.props.navigation.navigate('SignUpScreen')}>
-            <Text style={{padding:15, fontSize:20, color: '#b354e9',fontWeight:'bold', fontFamily:'OpenSans-ExtraBold'}}>SignUp</Text>
-            </TouchableHighlight>
-          </CardItem>
-    </Container> 
+
+      <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+        {
+          this.state.fontLoaded
+            ? (
+              <View>
+                <Text style={styles.neighborCorps}>Neighbor Corps</Text>
+                <Text style={styles.lendA}>Lend a hand for a better neighborhood.</Text>
+              </View>
+            )
+            : null
+        }
+
+        <Image source={require('../assets/images/PKLogo_transparent.png')} style={styles.logo} />
+
+      </View>
     )
   }
-}
+} 
